0ef464ddd7f2e6ca7f34c4f773a91c0734c0db8f
yashkatta <yash.katta1@gmail.com>
2019-05-15 20:05:01 +0530

    Initial commit flutter tutorial project startup_namer

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestrions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Startup New Generator"),),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /* 1 */ (context, i) {
+          if (i.isOdd) return Divider(); /* 2 */
+          final index = i ~/ 2;
+          if (index >= _suggestrions.length) {
+            _suggestrions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestrions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
